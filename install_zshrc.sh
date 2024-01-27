5382c6014d531a66a628cf6bc3afaa96b0253d56
Robert Bruce <entegral@protonmail.com>
2024-01-27 15:30:49 -0800

    rm default zshrc

--- install_zshrc.sh
+++ install_zshrc.sh
@@ -9,6 +9,9 @@ cd ~/dotfiles
 # Clone the zshrc repository using SSH
 git clone git@github.com:$github_username/zshrc.git
 
+# Remove the existing .zshrc file in the home directory
+rm -rf ~/.zshrc
+
 # Create a symbolic link to the .zshrc file in the home directory
 ln -s $PWD/zshrc/.zshrc ~/
 
